cbe3975e73214586c47791a2451069890601795f
calixd7 <calixd7@gmail.com>
2021-08-30 17:16:13 -0400

    fetching people and planets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState ([]);
+  const [planets, setPlanets] = useState ([]);
+  const [loading, setLoading] = useState (true);
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let res = await fetch('https://swapi.dev/api/people/?format=json');
+      let data = await res.json();
+      setPeople(data.results);
+    }
+  
+    async function fetchPlanets () { 
+      let res = await fetch('https://swapi.dev/api/planets/?format=json');
+      let data = await res.json();
+      setPlanets(data.results);
+    }
+
+    fetchPeople();
+    fetchPlanets();
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     hello
+   </div>
   );
 }
-
 export default App;
