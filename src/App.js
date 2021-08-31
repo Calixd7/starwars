53e79524c4bb9332799c581c8fa67457d66b5773
calixd7 <calixd7@gmail.com>
2021-08-30 20:05:05 -0400

    people component grid completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import { Container, Dimmer, Loader } from 'semantic-ui-react';
 import Home from './components/Home';
 import People from './components/People';
 import Planets from './components/Planets';
@@ -27,23 +27,30 @@ function App() {
 
     fetchPeople();
     fetchPlanets();
+    setLoading(false);
 
   }, []);
   return <div className="App">
     <Router>
     <Navbar />
     <Container>
-      <Switch> 
+      {loading ? (
+        <Dimmer active inverted> 
+        <Loader inverted>Loading</Loader>
+        </Dimmer>
+      ) : (<Switch> 
         <Route exact path= "/">
           <Home />
         </Route>
         <Route exact path= "/people">
-        <People />
+        <People data={people}/>
         </Route>
         <Route exact path= "/planets">
-          <Planets />
+          <Planets data={planets}/>
         </Route>
       </Switch>
+      )}
+      
     </Container>
 
     </Router>
--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';import { Card, Grid } from 'semantic-ui-react';
 
-export default function People() {
+
+export default function People({ data }) {
     return (
-        <div>
-            <p>People</p>
-        </div>
-    )
-}
+    <>
+           <h1>People</h1>
+           <Grid columns= {3}>
+               {data.map((people, i) => {
+                   return (
+                       <Grid.Column key={i}>
+                       <Card>
+                       <Card.Content>
+                       <Card.Header>{people.name}</Card.Header>
+                       <Card.Description>
+                           <strong>Height</strong>
+                           <p>{people.height}</p>
+                           <strong>Mass</strong>
+                           <p>{people.mass}</p>
+                           <strong>Hair Color</strong>
+                           <p>{people.hair_color}</p>
+                       </Card.Description>
+                       </Card.Content>
+                       </Card> 
+                       </Grid.Column>
+                    );
+                  })}
+           </Grid>    
+         </>
+        );
+       }
